6ec92a45ed46eb12f9675f8a5d20d47364003307
Diego Ivan <diego_cundapi@hotmail.com>
2024-10-10 20:50:45 -0600

    Datos del total de la compra

--- js/_actualizarCarrito.js
+++ js/_actualizarCarrito.js
@@ -1,6 +1,3 @@
-import { Cart } from "./cart.js";
-
-const carrito2 = new Cart();
 export function actualizarCarrito(carrito, carritoContent, contadorCarrito) {
   // Limpiar el contenido del carrito antes de actualizar
   while (carritoContent.firstChild) {
@@ -88,7 +85,7 @@ export function actualizarCarrito(carrito, carritoContent, contadorCarrito) {
     // Añadir el SVG al botón
     CancelarButton.append(svgIcon);
 
-    CancelarButton.addEventListener('click', () => {
+    CancelarButton.addEventListener("click", () => {
       carrito.deleteProductCart(product);
     });
 
@@ -100,6 +97,7 @@ export function actualizarCarrito(carrito, carritoContent, contadorCarrito) {
     carritoContent.append(sectionCarrito);
   });
 
+  // implementacion del precio total del carrito
   const totalContent = document.createElement("div");
   totalContent.className = "totalContent";
 
@@ -108,4 +106,34 @@ export function actualizarCarrito(carrito, carritoContent, contadorCarrito) {
 
   const totalPrice = document.createElement("p");
   totalPrice.className = "totalPrice";
+
+  const contentMessage = document.createElement("div");
+  contentMessage.className = "contentMessage";
+  const message = document.createElement("p");
+  message.className = "message";
+  // Crear un parser para el SVG
+  const parser = new DOMParser();
+  const svgText = `
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+    <path fill="#1EA575" d="M8 18.75H6.125V17.5H8V9.729L5.803 8.41l.644-1.072 2.196 1.318a1.256 1.256 0 0 1 .607 1.072V17.5A1.25 1.25 0 0 1 8 18.75Z"/><path fill="#1EA575" d="M14.25 18.75h-1.875a1.25 1.25 0 0 1-1.25-1.25v-6.875h3.75a2.498 2.498 0 0 0 2.488-2.747 2.594 2.594 0 0 0-2.622-2.253h-.99l-.11-.487C13.283 3.56 11.769 2.5 9.875 2.5a3.762 3.762 0 0 0-3.4 2.179l-.194.417-.54-.072A1.876 1.876 0 0 0 5.5 5a2.5 2.5 0 1 0 0 5v1.25a3.75 3.75 0 0 1 0-7.5h.05a5.019 5.019 0 0 1 4.325-2.5c2.3 0 4.182 1.236 4.845 3.125h.02a3.852 3.852 0 0 1 3.868 3.384 3.75 3.75 0 0 1-3.733 4.116h-2.5V17.5h1.875v1.25Z"/>
+  </svg>
+`;
+  const svgDoc = parser.parseFromString(svgText, "image/svg+xml");
+  const svgElement = svgDoc.documentElement;
+
+  // Añadir el SVG y el texto al mensaje
+  message.appendChild(svgElement);
+  message.appendChild(
+    document.createTextNode(" This is a carbon-neutral delivery")
+  );
+  let sumador = 0;
+  carrito.products.forEach((product) => {
+    sumador += product[0].price * product[1];
+  });
+  totalText.textContent = "Order Total";
+  totalPrice.textContent = `$${sumador.toFixed(2)}`;
+
+  contentMessage.append(message);
+  totalContent.append(totalText, totalPrice);
+  carritoContent.append(totalContent, contentMessage);
 }
